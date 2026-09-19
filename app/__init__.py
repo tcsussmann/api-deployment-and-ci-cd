b1299b7ade7e6e25cc71b7de0336926dd27f6de2
Timothy C Sussmann <timothy.curtiss@gmail.com>
2026-09-19 11:33:44 -0400

    add swagger ui to production

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 import bcrypt
 from flask import Flask
+from flask_swagger_ui import get_swaggerui_blueprint
 from .extensions import db, ma, limiter, cache, jwt, bcrypt
 from .blueprints.customer import customer_bp
 from .blueprints.mechanic import mechanic_bp
@@ -9,7 +10,25 @@ from .blueprints.inventory import inventory_bp
 
 def create_app(config_name):
     app = Flask(__name__)
+
+    SWAGGER_URL = '/api/docs'
+    API_URL = '/static/swagger.yaml'
+
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name': 'Mechanic Shop API'
+        }
+    )
+
+    app.register_blueprint(
+        swaggerui_blueprint,
+        url_prefix=SWAGGER_URL
+    )
+
     app.config.from_object(f'config.{config_name}')
+
 
     db.init_app(app)
     ma.init_app(app)
